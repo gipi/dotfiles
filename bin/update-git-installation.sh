2cb86bc33f4b67f469b0af434b4bea7594456260
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-05-11 18:03:27 +0200

    Clean up.

--- bin/update-git-installation.sh
+++ bin/update-git-installation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# TODO: dependencies check (asciidoc)
+
 die() {
 	echo -e "fatal: $1"
 	exit $2
@@ -16,9 +18,13 @@ SOURCE_PATH=/usr/src/git/
 cd ${SOURCE_PATH} || {
 	die "the source code repository doesn't exist at '$SOURCE_PATH', clone it at 'git://github.com/git/git.git'" 1
 }
+
 git fetch
 git checkout -f $REV || exit 1
 make configure
 ./configure --prefix=/opt/git-${REV} || exit 1
-make -j 2 && make install && make -j 2 doc && make install-doc && \
+make -j 2 && \
+	make install && \
+	make -j 2 doc && \
+	make install-doc && \
 	cd /opt/ && unlink git ; ln -s git-${REV} git
