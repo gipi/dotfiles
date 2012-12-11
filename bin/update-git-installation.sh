e13e08faafa7fb5b35e5b5d9589cbfa1d21b684a
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-12-11 12:12:54 +0100

    update-git: add --update switch.

--- bin/update-git-installation.sh
+++ bin/update-git-installation.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 # TODO: dependencies check (asciidoc)
-
-die() {
-	echo -e "fatal: $1"
-	exit $2
-}
-
-if [ -z "$1" ]
-then
-	echo "I need a revision (e.g. a tag like 'v1.7.10.1')"
-	exit 1
-fi
-
 REV=$1
 SOURCE_PATH=/usr/src/git/
 PREFIX=/opt/git-${REV}
@@ -20,7 +8,34 @@ cd ${SOURCE_PATH} || {
 	die "the source code repository doesn't exist at '$SOURCE_PATH', clone it at 'git://github.com/git/git.git'" 1
 }
 
-git fetch
+
+die() {
+	echo -e "fatal: $1"
+	exit $2
+}
+
+update() {
+    git fetch
+}
+
+usage() {
+    echo "$0 [--update | revision]"
+
+}
+
+if [ -z "$1" ]
+then
+	echo "I need a revision (e.g. a tag like 'v1.7.10.1')"
+    usage
+	exit 1
+fi
+
+if [ "$1" == "--update" ]
+then
+    update
+    exit 0
+fi
+
 git checkout -f $REV || exit 1
 make configure
 ./configure --prefix=$PREFIX || exit 1
